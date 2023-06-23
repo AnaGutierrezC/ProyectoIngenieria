8d36921bb0af6e6785da65579c5dc4e3933f2992
AnaGutierrezC <anacatalinagutierrez84@gmail.com>
2023-06-23 11:36:27 -0600

    Cambios en las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MysqlController;
+use App\Http\Controllers\UsuController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TicketsController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\EtiquetaController;
+use App\Http\Controllers\PrioridadController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +24,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios/registro',[UsuController::class, 'create']);
+Route::resource('/usuarios',UsuController::class);
+
+Route::resource('/ticketeria',TicketsController::class);
+Route::get('/ticketeria/crear',[TicketsController::class, 'create']);
+Route::get('/ticketeria/confirmacion',[TicketsController::class, 'store']);
+
+Route::get('/categorias',[CategoriaController::class, 'index']);
+
+
+Route::get('/etiquetas',[EtiquetaController::class, 'index']);
+
+Route::get('/prioridades',[PrioridadController::class, 'index']);
